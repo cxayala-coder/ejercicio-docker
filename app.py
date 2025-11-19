b6a841029529095c89fbe6452021ee151d8aeda3
Cesar Ayala S. <cxayala@gmail.com>
2025-11-18 19:12:43 -0500

    version actualizada de alpine

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ Script simple que imprime Hola Mundo
 """
  
 if __name__ == "__main__":
-    print("Hola Mundo de Cesar esta nueva version 3")
+    print("ESTA ES UNA NUEVA VERSION CON ALPINE ACTUALIZADO A V3.15")
